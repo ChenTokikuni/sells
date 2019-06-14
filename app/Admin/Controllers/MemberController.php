969367f111b6cb8b414f575d92dfedfe3fccee27
smallkevin <fortis013@gmail.com>
2019-06-14 14:12:19 +0800

    暫存

--- app/Admin/Controllers/MemberController.php
+++ app/Admin/Controllers/MemberController.php
@@ -322,7 +322,7 @@ class MemberController extends Controller
 		
 		$form->datetime('registration_date', '注册日期')->format('YYYY/MM/DD HH:mm');
 		
-		$form->datetime('last_login', '最后登录日');
+		$form->datetime('last_login', '最后登录日')->format('YYYY/MM/DD HH:mm');
 		
 		$form->number('offline_days', '多久没登录')->default('0')->min('0');
 
@@ -370,9 +370,13 @@ class MemberController extends Controller
 			
 			setlocale(\LC_ALL, 'en_US.UTF-8');
 			// Bulk insert
+			
 			while ($rows = $this->getCsvContents($handle)) {
+				
 				$sql = "INSERT INTO member ( account, name, phone_number, mail, qq_number, bank_number, save_count, pay_count, total_save, total_pay, registration_date, last_login, offline_days, created_at, updated_at ) VALUES ";
+				
 				$values = array_fill(0, count($rows), "( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW() )");
+				//print_r($values);exit;
 				$binds = []; 							
 				foreach ($rows as $row) {
 					for ($i = 0; $i < 13; $i++) {
@@ -380,7 +384,7 @@ class MemberController extends Controller
 					}
 				}
 				$sql .= implode(', ', $values);
-				
+
 				$sql .= " ON DUPLICATE KEY UPDATE account =VALUES(account), updated_at =NOW()";
 				\Illuminate\Support\Facades\DB::insert($sql, $binds);
 			}
